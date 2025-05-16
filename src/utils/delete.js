7e8e9e4057792034043b65c97c40450c69d74388
_Anuj <ansinghch@gmail.com>
2025-05-16 10:59:43 +0530

    ..

---
+++ src/utils/delete.js
@@ -0,0 +1,12 @@
+let arr1 = [1, 2, 3, 4, 5];
+
+// let array = arr1.map((i) => {
+//     return i * 2;
+// })
+
+let array = arr1.forEach((i) => {
+    return i * 2;
+})
+console.log(array);
+console.log(arr1);
+
